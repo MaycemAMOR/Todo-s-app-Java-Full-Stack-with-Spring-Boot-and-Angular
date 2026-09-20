850f90512ef19e6c2b038d14cec5530c23ec34e9
Maycem AMOR <maycemamor@gmail.com>
2026-09-20 19:48:37 +0200

    update listTodo components.

--- todo-ui/src/app/app.routes.ts
+++ todo-ui/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import {Routes} from '@angular/router';
 import {Welcome} from './welcome/welcome';
 import {Login} from './login/login';
 import {Error} from './error/error';
+import {ListTodos} from './list-todos/list-todos';
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
   {
     path: 'login',
     component: Login
+  },
+  {
+    path: 'todos',
+    component: ListTodos
   },
   {
     path: '**',
--- todo-ui/src/app/list-todos/list-todos.ts
+++ todo-ui/src/app/list-todos/list-todos.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  imports: [],
+  imports: [
+  ],
   selector: 'app-list-todos',
   styleUrl: './list-todos.css',
   templateUrl: './list-todos.html',
+  standalone: true
 })
-export class ListTodos {}
+export class ListTodos {
+
+  todos= [
+    {id: 1, description: 'learn to Dance'},
+    {id: 2, description: 'Become an expert at Angular'},
+    {id: 3, description: 'learn react'},
+    {id: 4, description: 'learn Spring boot'},
+    {id: 5, description: 'Visit India'}
+  ]
+
+  constructor() {
+  }
+
+}
