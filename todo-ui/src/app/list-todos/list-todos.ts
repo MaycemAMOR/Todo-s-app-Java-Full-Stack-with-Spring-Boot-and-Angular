705dd35708e7cc59c40c6f18a9892d261df92944
Maycem AMOR <maycemamor@gmail.com>
2026-09-20 20:16:15 +0200

    update Todos Component using interface ans pipes

--- todo-ui/src/app/list-todos/list-todos.ts
+++ todo-ui/src/app/list-todos/list-todos.ts
@@ -1,21 +1,27 @@
 import {Component} from '@angular/core';
+import {Todo} from '../models/todo';
+import {DatePipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   imports: [
+    DatePipe,
+    UpperCasePipe
   ],
   selector: 'app-list-todos',
   styleUrl: './list-todos.css',
   templateUrl: './list-todos.html',
   standalone: true
 })
+
+
 export class ListTodos {
 
-  todos= [
-    {id: 1, description: 'learn to Dance'},
-    {id: 2, description: 'Become an expert at Angular'},
-    {id: 3, description: 'learn react'},
-    {id: 4, description: 'learn Spring boot'},
-    {id: 5, description: 'Visit India'}
+  todos: Todo[] = [
+    {id: 1, description: 'learn to Dance', done: false, targetDate: new Date()},
+    {id: 2, description: 'Become an expert at Angular', done: false, targetDate: new Date()},
+    {id: 3, description: 'learn react', done: false, targetDate: new Date()},
+    {id: 4, description: 'learn Spring boot', done: false, targetDate: new Date()},
+    {id: 5, description: 'Visit India', done: false, targetDate: new Date()},
   ]
 
   constructor() {
