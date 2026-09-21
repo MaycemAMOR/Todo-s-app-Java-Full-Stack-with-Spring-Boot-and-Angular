511c8db111d203a37c8ddfcd9eabd96638cbce68
Maycem AMOR <maycemamor@gmail.com>
2026-09-21 22:09:52 +0200

    Using Session Storage to Store User Authentication Token

--- todo-ui/src/app/services/hardcoded-authentication.ts
+++ todo-ui/src/app/services/hardcoded-authentication.ts
@@ -4,7 +4,23 @@ import {Service} from '@angular/core';
 export class HardcodedAuthentication {
 
   authenticate(username: string, password: string) {
+    console.log('isUserLoggedIn  before: ' + this.isUserLoggedIn());
+    console.log('this.authentication(username, password)  before: ' + this.authentication(username, password));
+    if (this.authentication(username, password)) {
+      sessionStorage.setItem('authenticateUser', username);
+      console.log('isUserLoggedIn  after: ' + this.isUserLoggedIn());
+    }
+    return this.authentication(username, password);
+  }
+
+  public authentication(username: string, password: string) {
     return username == "in28minutes" && password == 'dummy';
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticateUser');
+    console.log('user : ' + user);
+    return !(user === null)
   }
 }
 
