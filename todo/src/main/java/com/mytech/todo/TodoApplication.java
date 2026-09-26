6a4faaf9c7946564ed64798e0dbca1870b6473fd
Maycem AMOR <maycemamor@gmail.com>
2026-09-26 13:24:35 +0200

    Initializing a todol-web-services Project with Spring Boot

---
+++ todo/src/main/java/com/mytech/todo/TodoApplication.java
@@ -0,0 +1,13 @@
+package com.mytech.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TodoApplication.class, args);
+	}
+
+}
